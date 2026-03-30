81253523614c63fee219c19fac249ec6474874bb
impart-ua <impart.info@gmail.com>
2026-03-31 00:08:32 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>App Test v2</h1>
+      <p>React + Vite project is ready.</p>
+      <button onClick={() => alert('It works!')}>Test button</button>
+    </div>
+  )
+}
+
+export default App
