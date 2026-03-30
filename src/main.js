d3476ac3dafd0177038d3afd5e8d83547cb2a10a
impart-ua <impart.info@gmail.com>
2026-03-31 00:07:56 +0300

    Delete src/main.js

--- src/main.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("btn");
-const status = document.getElementById("status");
-
-button.addEventListener("click", () => {
-  status.textContent = "Button clicked. Project is alive.";
-});
